1a460a9d772e80713d17f9e91983fa023f6452bb
antareephasan <antareephasan@gmail.com>
2024-02-28 21:43:49 +0600

    added player

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { Figtree } from 'next/font/google'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
-import ToasterProvider from '@/hooks/ToasterProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 import getSongsByUserId from '@/actions/getSongsByUserId'
+import Player from '@/components/Player'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -33,6 +34,7 @@ export default async function RootLayout({
             <Sidebar songs={userSongs} >
               {children}
             </Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -2,6 +2,7 @@
 
 import LikeButton from '@/components/LikeButton';
 import MediaItem from '@/components/MediaItem';
+import useOnPlay from '@/hooks/useOnPlay';
 import { useUser } from '@/hooks/useUser';
 import { Song } from '@/types';
 import { useRouter } from 'next/navigation';
@@ -15,6 +16,8 @@ const LikedContent: React.FC<LikedContentProps> = ({
 }) => {
     const router = useRouter();
     const { isLoading, user } = useUser();
+
+    const onPlay = useOnPlay(songs);
 
     useEffect(() => {
         if (!isLoading && !user) {
@@ -40,7 +43,7 @@ const LikedContent: React.FC<LikedContentProps> = ({
                     <div className='flex-1'>
                         <MediaItem
                             data={song}
-                            onClick={() => { }}
+                            onClick={(id: string) => onPlay(id)}
                         />
                     </div>
 
